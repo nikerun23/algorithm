a8a7ac5127d0034ea66134b500a86592542bc68b
HK Lee <nikerun23@naver.com>
2019-02-13 18:34:50 +0900

    최대값, 최소값 구하기

--- bookStudy/chapter01/Max3.java
+++ bookStudy/chapter01/Max3.java
@@ -2,8 +2,41 @@ package algorithm.bookStudy.chapter01;
 
 import java.util.Scanner;
 
+// 최댓값, 최소값을 구하여 나타냄
 public class Max3 {
 
+    // a, b, c의 최댓값을 구하여 반환
+    private static int max3(int a, int b, int c) {
+        int max = a;
+        if (b > max) max = b;
+        if (c > max) max = c;
+        return max;
+    }
+    
+    // a, b, c, d의 최댓값을 구하여 반환
+    private static int max4(int a, int b, int c, int d) {
+        int max = a;
+        if (b > max) max = b;
+        if (c > max) max = c;
+        if (d > max) max = d;
+        return max;
+    }
+    // a, b, c의 최소값을 구하여 반환
+    private static int min3(int a, int b, int c) {
+        int min = a;
+        if (b < min) min = b;
+        if (c < min) min = c;
+        return min;
+    }
+    // a, b, c, d의 최소값을 구하여 반환
+    private static int min4(int a, int b, int c, int d) {
+        int min = a;
+        if (b < min) min = b;
+        if (c < min) min = c;
+        if (d < min) min = d;
+        return min;
+    }
+    
     public static void main(String[] args) {
         
         System.out.println("세 정수의 최댓값을 구합니다");
@@ -22,7 +55,12 @@ public class Max3 {
         if (b > max) max = b;
         if (c > max) max = c;
         
-        System.out.print("Max 값은 : " + max);
+        System.out.println("입력한 정수의 Max 값은 : " + max);
+        
+        System.out.println("1,2,3 Max 값은 : " + max3(1,2,3));
+        System.out.println("1,2,3,4 Max 값은 : " + max4(1,2,3,4));
+        System.out.println("1,2,3 Min 값은 : " + min3(1,2,3));
+        System.out.println("1,2,3,4 Min 값은 : " + min4(1,2,3,4));
 
     }
 
