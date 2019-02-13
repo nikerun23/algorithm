8ad8a54e297ad7e1c4868d9f462362ae5b1daf54
HK Lee <nikerun23@naver.com>
2019-02-13 16:31:51 +0900

    세 값의 최대값 구하기

---
+++ chapter01/Max3.java
@@ -0,0 +1,29 @@
+package algorithm.chapter01;
+
+import java.util.Scanner;
+
+public class Max3 {
+
+    public static void main(String[] args) {
+        
+        System.out.println("세 정수의 최댓값을 구합니다");
+        
+        Scanner stdid = new Scanner(System.in);
+        
+        System.out.print("a : ");
+        int a = stdid.nextInt();
+        System.out.print("b : ");
+        int b = stdid.nextInt();
+        System.out.print("c : ");
+        int c = stdid.nextInt();
+        
+        int max = a;
+        
+        if (b > max) max = b;
+        if (c > max) max = c;
+        
+        System.out.print("Max 값은 : " + max);
+
+    }
+
+}
