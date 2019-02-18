f1a614b5e72cf5a6ab8064c3637ef9d662b86668
HK Lee <nikerun23@naver.com>
2019-02-18 17:57:19 +0900

    두 배열의 비교 및 배열 복사

--- bookStudy/chapter02/ArrayEqual.java
+++ bookStudy/chapter02/ArrayEqual.java
@@ -1,6 +1,6 @@
 package algorithm.bookStudy.chapter02;
 
-// 두 배열의 비교
+// 두 배열의 비교, 배열 Copy
 public class ArrayEqual {
 
 	static boolean equals(int[] a, int[] b) {
@@ -14,10 +14,20 @@ public class ArrayEqual {
 		return true;
 	}
 	
+	static int[] copy(int[] a) {
+		int[] result = new int[a.length];
+		
+		for (int i = 0, len = a.length; i < len; i++) {
+			result[i] = a[i];
+		}
+		
+		return result;
+	}
+	
 	public static void main(String[] args) {
 
 		int[] a = {23, 7, 10, 2};
-		int[] b = {23, 7, 10, 2};
+		int[] b = copy(a);
 		if (equals(a, b)) {
 			System.out.println("배열 a와 배열 b는 같습니다");
 		} else {
