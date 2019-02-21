503c4ec4544d4e0ef5b1a45f2a8fa6374bd2ac53
HK Lee <nikerun23@naver.com>
2019-02-21 17:53:17 +0900

    알고리즘 개선 두번째 (계산횟수 78,022  > 14,622 > 1324)

--- bookStudy/chapter02/PrimeNumber1.java
+++ bookStudy/chapter02/PrimeNumber1.java
@@ -10,23 +10,27 @@ public class PrimeNumber1 {
         int[] prime = new int[500]; // 소수를 저장할 배열
         
         prime[ptr++] = 2; // 2는 소수임
+        prime[ptr++] = 3; // 3은 소수임
         
-        for (int n = 3; n <= 1000; n+=2) { // 대상은 홀수만
-            int i;
-            for (i = 1; i < ptr; i++) { // 이미 찾은 소수로 나누어 봄
-                counter++;
+        for (int n = 5; n <= 1000; n+=2) { // 대상은 홀수만
+            boolean flag = false;
+            for (int i = 1; prime[i] * prime[i] <= n; i++) {
+                counter += 2;
                 if (n % prime[i] == 0) // 나누어떨어지면 소수가 아님
-                    break; // 더 이상의 반복은 불필요
+                    flag = true;
+                    break; // 합성수이므로 더 이상의 반복은 불필요
             }
-            if (ptr == i) // 마지막까지 나누어 떨어지지 않음
+            if (!flag) { // 마지막까지 나누어 떨어지지 않음
                 prime[ptr++] = n; // 소수라고 배열에 저장
+                counter++;
+            }
         }
         
         for (int i = 0; i < ptr; i++) {
             System.out.println(prime[i]);
         }
         
-        System.out.println("나눗셈을 수행한 수 : " + counter); // 알고리즘 개선전 = 78,022 / 개선후 = 14,622
+        System.out.println("곱셈과 나눗셈을 수행한 수 : " + counter); // 알고리즘 개선전 = 78,022 / 개선후 = 14,622 / 개선후 = 1324
     }
 
 }
