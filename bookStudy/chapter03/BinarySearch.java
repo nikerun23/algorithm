9ad7d32bec44ec5f28a4467351a041f56e2cf9af
HK Lee <nikerun23@naver.com>
2019-03-10 20:36:38 +0900

    검색 알고리즘 - 선형검색 (검색 요소의 갯수 구하기)

--- bookStudy/chapter03/BinarySearch.java
+++ bookStudy/chapter03/BinarySearch.java
@@ -1,10 +1,25 @@
 package algorithm.bookStudy.chapter03;
 
-import java.lang.reflect.Array;
 import java.util.Scanner;
 
 // 이진검색
 public class BinarySearch {
+	
+	// 요숫수가 n인 배열 a에서 key와 같은 모든 요소를 검색합니다.
+	static int searchIdx(int[] a, int key) {
+		
+		int[] idx = new int[a.length];
+		int count = 0;
+		
+		for (int i = 0; i < a.length-1; i++) {
+			if (a[i] == key) {
+				idx[count] = a[i];
+				count++;
+			}
+		}
+		
+		return count;
+	}
 
 	// 요숫수가 n인 배열 a에서 key와 같은 요소를 이진 검색합니다.
 	static int binSearch(int[] a, int n, int key) {
@@ -37,10 +52,13 @@ public class BinarySearch {
 		
 		System.out.print("검색할 값 : "); // 키값을 입력
 		int key = stdIn.nextInt();
-		int idx = binSearch(x, n, key); // 배열 x에서 키 값이 key인 요소를 검색
+//		int idx = binSearch(x, n, key); // 배열 x에서 키 값이 key인 요소를 검색
+		int count = searchIdx(x, key); // 배열 x에서 키 값이 key인 요소의 갯수를 검색
 
-		if (idx == -1) System.out.print("그 값의 요소가 없습니다.");
-		else if (idx != -1) System.out.print(key + "는 x[" +idx+ "]에 있습니다");
+		if (count == 0) System.out.print("그 값의 요소가 없습니다.");
+		else if (count > 0) System.out.print(key + "는 "+count+"개 있습니다");
+//		if (idx == -1) System.out.print("그 값의 요소가 없습니다.");
+//		else if (idx != -1) System.out.print(key + "는 x[" +idx+ "]에 있습니다");
 		
 	}
 
