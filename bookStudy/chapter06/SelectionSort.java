7499b85ffd410d181f6475c0806d84434bb100e6
HK Lee <nikerun23@naver.com>
2019-05-06 20:54:59 +0900

    정렬 - 단순 선택 정렬

---
+++ bookStudy/chapter06/SelectionSort.java
@@ -0,0 +1,44 @@
+package algorithm.bookStudy.chapter06;
+
+// 단순 선택 정렬
+public class SelectionSort {
+
+	// 배열 요소 a[idx1]과 a[idx2]의 값을 바꿉니다.
+		static void swap(int[] a, int idx1, int idx2) {
+			int t = a[idx1]; a[idx1] = a[idx2]; a[idx2] = t;
+		}
+
+		// 단순 선택 정렬
+		static void selectionSort(int[] a, int n) {
+			for (int i = 0; i < n - 1; i++) {
+				int min = i;			// 아직 정렬되지 않은 부분에서 가장 작은 요소의 인덱스를 기록합니다.
+				for (int j = i + 1; j < n; j++)
+					if (a[j] < a[min])
+						min = j;
+				swap(a, i, min);		// 아직 정렬되지 않은 부분의 첫 요소와 가장 작은 요소를 교환합니다.
+			}
+		}
+
+		public static void main(String[] args) {
+			
+			int [] a = {23, 7, 10, 1, 3};
+
+			int n = a.length;
+			System.out.print("int[] a = {");
+			for (int i = 0, len = a.length; i < len; i++) {
+				System.out.print(a[i]+", ");
+			}
+			System.out.println("}");
+			System.out.println("오름 차순으로 정렬합니다");
+			
+			selectionSort(a, n);			// 배열x를 단순선택정렬
+			
+			System.out.print("int[] a = {");
+			for (int i = 0; i < a.length; i++) {
+				System.out.print(a[i]+", ");
+			}
+			System.out.println("}");
+
+		}
+
+}
